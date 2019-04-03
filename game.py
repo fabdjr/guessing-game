a40cff39ccb5eb4542baa4935e3e98e000cb8910
Fabricio Darosci Junior <fabricio@sbtur.com>
2019-04-03 16:00:52 -0300

    Adds user input capability

--- game.py
+++ game.py
@@ -1,1 +1,8 @@
-print("Welcome to the Guessing Game")
+print("####################################")
+print("##  Welcome to the Guessing Game  ##")
+print("####################################")
+
+secret_number = 42
+
+guessing = input("Take a good guess:")
+print("You choose: ", guessing)
