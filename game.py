4182b0ff17b71b4317469de620958c2b65dc496b
Fabricio Darosci Junior <fabricio@sbtur.com>
2019-04-03 17:28:43 -0300

    Adds round logic

--- game.py
+++ game.py
@@ -3,23 +3,32 @@ print("##  Welcome to the Guessing Game  ##")
 print("####################################")
 
 secret_number = 42
+round = 1
+rounds_limit = 3
 
-guessing_number_str = input("Take a good guess: ")
-print("You choose: ", guessing_number_str)
+while(round <= rounds_limit):
+    print("Round", round, "from", rounds_limit)
 
-# typecast is required as long as input() returns a str
-guessing_number = int(guessing_number_str)
+    guessing_number_str = input("Take a good guess: ")
+    print("You choose: ", guessing_number_str)
 
-guessing_is_correct = guessing_number == secret_number
-guessing_is_higher = guessing_number > secret_number
-guessing_is_lower = guessing_number < secret_number
+    # typecast is required as long as input() returns a str
+    guessing_number = int(guessing_number_str)
 
-if guessing_is_correct:
-    print("WOW! You guessed it!")
-else:
-    if(guessing_is_higher):
-        print("OPS! Not this time: your guessing is HIGHER than the secret number.")
-    elif(guessing_is_lower): # this line doesn't make any logic sense, just for syntax demonstration
-        print("OPS! Not this time: your guessing is LOWER than the secret number.")
+    guessing_is_correct = guessing_number == secret_number
+    guessing_is_higher = guessing_number > secret_number
+    guessing_is_lower = guessing_number < secret_number
+
+    if guessing_is_correct:
+        print("WOW! You guessed it!")
+    else:
+        if(guessing_is_higher):
+            print("OPS! Not this time: your guessing is HIGHER than the secret number.")
+        elif(guessing_is_lower): # this line doesn't make any logic sense, just for syntax demonstration
+            print("OPS! Not this time: your guessing is LOWER than the secret number.")
+
+    round = round + 1
+
+    print("####################################")
 
 print("Game end!")
