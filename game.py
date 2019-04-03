2b930c97db589362c2a67eccd47f5118a96094d4
Fabricio Darosci Junior <fabricio@sbtur.com>
2019-04-03 16:53:03 -0300

    Adds number comparison logic

--- game.py
+++ game.py
@@ -4,5 +4,13 @@ print("####################################")
 
 secret_number = 42
 
-guessing = input("Take a good guess:")
-print("You choose: ", guessing)
+guessing_number_str = input("Take a good guess: ")
+print("You choose: ", guessing_number_str)
+
+# typecast is required as long as input() returns a str
+guessing_number = int(guessing_number_str)
+
+if secret_number == guessing_number:
+    print("WOW! You guessed it!")
+else:
+    print("OPS! Not this time!")
