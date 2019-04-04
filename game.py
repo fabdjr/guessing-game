53a61b1a65be676d9ff6b708def556049a51ac73
Fabricio Darosci Junior <fabricio@sbtur.com>
2019-04-04 13:24:29 -0300

    Adds game levels

--- game.py
+++ game.py
@@ -1,15 +1,25 @@
 import random
 
+secret_number = random.randrange(1, 101)
+
 print("####################################")
 print("##  Welcome to the Guessing Game  ##")
 print("####################################")
+print("## (1) Easy  (2) Medium  (3) Hard ##")
+print("####################################")
 
-secret_number = random.randrange(1, 101)
-rounds_limit = 3
+level_str = input("Select your level: ")
+level = int(level_str)
 
-print(secret_number)
+if level == 1:
+    rounds_limit = 20
+elif level == 2:
+    rounds_limit = 10
+else:
+    rounds_limit = 5
 
 for current_round in range(1, rounds_limit + 1):
+    print("####################################")
     print("Round {} from {}".format(current_round, rounds_limit))
 
     guessing_number_str = input("Take a good guess (between 1 and 100): ")
@@ -35,6 +45,7 @@ for current_round in range(1, rounds_limit + 1):
         elif guessing_is_lower:  # this line doesn't make any logic sense, just for syntax demonstration
             print("OPS! Not this time: your guessing is LOWER than the secret number.")
 
-    print("####################################")
 
-print("Game end!")
+print("####################################")
+print("##           Game end!            ##")
+print("####################################")
