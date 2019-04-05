2cc44c057196f2dcceccde2fe5ac15fbc88166a6
Fabricio Darosci Junior <fabricio@sbtur.com>
2019-04-05 16:56:45 -0300

    Adds letter search logic

--- hangman.py
+++ hangman.py
@@ -8,8 +8,14 @@ def play():
     guessed = False
 
     while(not hanged and not guessed):
-        letter = input("Choose a letter: ")
-        print("Playing...")
+        chosen_letter = input("Choose a letter: ")
+        index = 0
+
+        for letter in secret_word:
+            if letter == chosen_letter:
+                print("Found letter '{}' at {}".format(chosen_letter, index))
+
+            index = index + 1
 
     print("###########################################")
     print("##              Game end!                ##")
