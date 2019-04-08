50c051183a9700eb6cae5be4ea719b8b905c11dc
Fabricio Darosci Junior <fabricio@sbtur.com>
2019-04-08 14:13:50 -0300

    Adds win/lose logic to hangman

--- hangman.py
+++ hangman.py
@@ -5,24 +5,38 @@ def play():
 
     secret_word = "banana"
     masked_word = ["_", "_", "_", "_", "_", "_"]
+    attempts = 0
 
     hanged = False
     guessed = False
 
+    print(masked_word)
+
     while(not hanged and not guessed):
-        print(masked_word)
+
+
         chosen_letter = input("Choose a letter: ")
         chosen_letter = chosen_letter.strip()
+
         index = 0
+        if chosen_letter.upper() in secret_word.upper():
+            for letter in secret_word:
+                if letter.upper() == chosen_letter.upper():
+                    masked_word[index] = letter.upper()
+                index += 1
+        else:
+            attempts += 1
 
-        for letter in secret_word:
-            if letter.upper() == chosen_letter.upper():
-                masked_word[index] = letter.upper()
+        print(masked_word)
 
-            index = index + 1
+        hanged = attempts == 6
+        guessed = "_" not in masked_word
 
     print("###########################################")
-    print("##              Game end!                ##")
+    if guessed:
+        print("##              YOU WIN!                 ##")
+    else:
+        print("##              GAME OVER                ##")
     print("###########################################")
 
 
